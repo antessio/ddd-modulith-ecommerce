d6af55abd2e95fdabdcbf32142427dd1686da629
Antonio D'Alessio <antessio7@gmail.com>
2023-01-11 22:20:21 +0100

    initial setup

---
+++ application/src/main/java/antessio/dddmodulith/ecommerce/App.java
@@ -0,0 +1,13 @@
+package antessio.dddmodulith.ecommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
